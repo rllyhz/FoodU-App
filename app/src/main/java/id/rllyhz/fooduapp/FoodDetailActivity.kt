b70f1dddaa116eeaf38cebe44252213a7ee7cc1d
rllyhz <rullyihza00@gmail.com>
2021-03-06 08:50:00 +0700

    Updating FoodDetailActivity.kt

--- app/src/main/java/id/rllyhz/fooduapp/FoodDetailActivity.kt
+++ app/src/main/java/id/rllyhz/fooduapp/FoodDetailActivity.kt
@@ -7,5 +7,13 @@ class FoodDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_detail)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        TODO("Not yet implemented")
     }
 }
