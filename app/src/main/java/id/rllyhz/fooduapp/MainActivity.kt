e9f0935df55da91dae42458015df1b1a7ff4aa51
rllyhz <rullyihza00@gmail.com>
2021-03-06 08:42:14 +0700

    Updating MainActivity.kt

--- app/src/main/java/id/rllyhz/fooduapp/MainActivity.kt
+++ app/src/main/java/id/rllyhz/fooduapp/MainActivity.kt
@@ -1,14 +1,33 @@
 package id.rllyhz.fooduapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.recyclerview.widget.RecyclerView
+import id.rllyhz.fooduapp.adapter.FoodAdapter
+import id.rllyhz.fooduapp.data.Food
+import id.rllyhz.fooduapp.helper.DataHelper
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), FoodAdapter.OnItemClickCallback {
+    private lateinit var rvFoodList: RecyclerView
+    private lateinit var foodAdapter: FoodAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_FoodUApp)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(R.layout.activity_main)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        // rvFoodList = findViewById(R.id.rv_food_list)
+        // foodAdapter = FoodAdapter(this, DataHelper.foodList)
+    }
+
+    override fun ctaItemClicked(food: Food) {
+        //
     }
 }
